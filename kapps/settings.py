4ee0ece6ac7acef46711dac5ef451118f0ba0072
Eddie Lutte <eddielutte@hotmail.com>
2018-09-16 19:06:51 +0100

    update of allowed_hosts setting to *

--- kapps/settings.py
+++ kapps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '59aeeeu-$4l6jbjbt!xs(78g8xs2&%$q7f^h6sp5^564)l9pc2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
